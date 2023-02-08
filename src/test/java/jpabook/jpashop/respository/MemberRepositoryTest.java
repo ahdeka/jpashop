0b80218b61013e43ce7da413ba8c573d9752febc
ahdeka <hdim@idevel.kr>
2023-02-08 17:49:00 +0900

    10강 5/6

--- src/test/java/jpabook/jpashop/respository/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/respository/MemberRepositoryTest.java
@@ -1,14 +1,11 @@
 package jpabook.jpashop.respository;
 
-import jpabook.jpashop.entity.Member;
+import jpabook.jpashop.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class MemberRepositoryTest {
@@ -21,7 +18,7 @@ class MemberRepositoryTest {
     public void testMember() throws Exception {
         //given
         Member member = new Member();
-        member.setUserName("memberA");
+        member.setName("memberA");
 
         //when
         Long savedId = memberRepository.save(member);
@@ -29,8 +26,6 @@ class MemberRepositoryTest {
 
         //then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
-
-
+        Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
     }
 }
