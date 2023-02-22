aa4bc9c7a482036ca2cd9afc83614aff7eb515f4
ahdeka <hdim@idevel.kr>
2023-02-22 17:19:33 +0900

    21강/36

---
+++ src/main/java/jpabook/jpashop/respository/ItemRepository.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.respository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) {
+            em.persist(item); // 없다면 새로 추가느낌
+        }else{
+            em.merge(item); // 있다면 merge = 업데이트 한다는 느낌
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class).getResultList();
+    }
+
+}
